0d4c85cb6ee14801dc4f2cbea54fb79a834bffae
Tomas Ferrari <tferrari.92@gmail.com>
2024-04-04 15:57:48 -0300

    implement AATT variables

--- OLD/X-test-docs-template-file/skeleton/README.md
+++
@@ -1,5 +0,0 @@
-# ${{ values.name }}
-
-${{ values.description }}
-
-Owned by ${{ values.owner }}
--- initial-setup.sh
+++ initial-setup.sh
@@ -20,7 +20,27 @@ replace_string_in_file() {
 replace_string_in_file "application-code/my-app/backend/catalog-info.yaml" "$github_username" "$dockerhub_username"
 replace_string_in_file "application-code/my-app/frontend/catalog-info.yaml" "$github_username" "$dockerhub_username"
 replace_string_in_file "application-code/my-app/redis/catalog-info.yaml" "$github_username" "$dockerhub_username"
-replace_string_in_file "backstage/helm-chart/values-custom.yaml" "$github_username" "$dockerhub_username"
+
+replace_string_in_file "argo-cd/applications/infra/backstage-application.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/infra/grafana-application.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/infra/prometheus-application.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/backend/application-dev.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/backend/application-prod.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/backend/application-stage.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/frontend/application-dev.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/frontend/application-prod.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/frontend/application-stage.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/redis/application-dev.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/redis/application-prod.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/applications/systems/my-app/redis/application-stage.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/self-manage/argocd-app-of-apps-application.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "argo-cd/self-manage/argocd-application.yaml" "$github_username" "$dockerhub_username"
+
 replace_string_in_file "backstage/my-backstage/app-config.yaml" "$github_username" "$dockerhub_username"
+
+replace_string_in_file "helm-charts/infra/backstage/values-custom.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "helm-charts/systems/my-app/backend/values.yaml" "$github_username" "$dockerhub_username"
+replace_string_in_file "helm-charts/systems/my-app/frontend/values.yaml" "$github_username" "$dockerhub_username"
+
 
 echo -n "That's it! All necessary files were updated with the info you provided. You can go back to the README and carry on with the guide."
